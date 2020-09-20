5c2d129c1befe8d8eb5f9eecd124dfa1401a1024
Marco Ceriani <marco-ceriani@users.noreply.github.com>
2020-09-20 14:26:10 +0200

    test with mockito ArgumentCaptor

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -32,13 +33,28 @@ public class IndexControllerTest {
 
     @Test
     public void name() {
+
+        // given
         Set<Recipe> mockedRecipes = new HashSet<>();
-        mockedRecipes.add(new Recipe());
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1l);
+        mockedRecipes.add(recipe1);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2l);
+        mockedRecipes.add(recipe2);
+
         when(recipeService.listRecipes()).thenReturn(mockedRecipes);
 
+        ArgumentCaptor<Set<Recipe>> argCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // when
         String templateName = indexController.getIndexPage(model);
+
+        // then
         assertEquals("index", templateName);
-        verify(model, times(1)).addAttribute("recipes", mockedRecipes);
         verify(recipeService, times(1)).listRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argCaptor.capture());
+        Set<Recipe> modelRecipes = argCaptor.getValue();
+        assertEquals(2, modelRecipes.size());
     }
 }
