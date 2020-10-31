cc69c7b10a66a4f067e45cf53367edcd7f75bebd
Marco Ceriani <marco-ceriani@users.noreply.github.com>
2020-10-31 12:02:17 +0100

    add back NmberFormatExceptionHandler, not sure if used at all

--- src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
@@ -1,21 +1,19 @@
 package guru.springframework.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    // TODO: replace this
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormatException(Exception exception) {
-//        log.error("Invalid number", exception);
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setStatus(HttpStatus.BAD_REQUEST);
-//        modelAndView.setViewName("error400");
-//        modelAndView.addObject("exception", exception);
-//        return modelAndView;
-//    }
+    @ExceptionHandler(NumberFormatException.class)
+    public String handleNumberFormatException(Exception exception, Model model) {
+        log.error("Binding error: Invalid number", exception.getMessage());
+        model.addAttribute("exception", exception);
+        return "error400";
+    }
 
 }
